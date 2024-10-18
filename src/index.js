63b296e62de0ec5d375fc6218c8ec7e885f6703b
Andrey <andreybess1488@gmail.com>
2024-10-18 14:16:41 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+<<<<<<< Updated upstream
 import './css/main.min.css'
+=======
+import '../src/css/main.min.css'
+>>>>>>> Stashed changes
 
 const app = ReactDOM.createRoot(document.getElementById('app'));
 app.render(
