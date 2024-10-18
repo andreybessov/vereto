bd4cf39041c4fe8b296c08d46fd86003a4df54a3
Andrey <andreybess1488@gmail.com>
2024-10-18 15:12:39 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './css/main.min.css'
+import './scss/main.scss'
 
 const app = ReactDOM.createRoot(document.getElementById('app'));
 app.render(
